c7f132522e5505bb6b4c9b60ba061d34f115714b
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-08 14:08:29 +0100

    Added helse-streams to composite build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "helse-monorepo"
 includeBuild("helse-soknadsvalidator")
 includeBuild("helse-sykepengebehandling")
 includeBuild("helse-sykepengesoknadfilter")
+includeBuild("helse-streams")
