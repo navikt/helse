4ab3ce133be5944fb864caba2546972818eb61eb
Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
2018-11-26 09:38:36 +0100

    added helse-spa to the super-repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "helse-monorepo"
 
 includeBuild("helse-sparkel")
 includeBuild("helse-spleis")
+includeBuild("helse-spa")
 includeBuild("helse-spinne")
 includeBuild("helse-splitt")
 includeBuild("helse-streams")
